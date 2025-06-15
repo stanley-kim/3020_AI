9f6da5b7546152b0d78c222e8c6e3be87fa33968
stanley <stanley.will.kim>
2025-06-15 20:58:51 +0900

    created copy-cw-envt.py

---
+++ MidTerm/copy-cw-envt.py
@@ -0,0 +1,166 @@
+import pybullet as p
+import pybullet_data
+import time
+import numpy as np
+import random
+import creature
+import simulation
+import population
+import genome
+
+# p.connect(p.GUI)
+# p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+import random
+#import pybullet as p
+import math
+#import pybullet as p
+
+
+
+def make_mountain(num_rocks=100, max_size=0.25, arena_size=10, mountain_height=5):
+    def gaussian(x, y, sigma=arena_size/4):
+        """Return the height of the mountain at position (x, y) using a Gaussian function."""
+        return mountain_height * math.exp(-((x**2 + y**2) / (2 * sigma**2)))
+
+    for _ in range(num_rocks):
+        x = random.uniform(-1 * arena_size/2, arena_size/2)
+        y = random.uniform(-1 * arena_size/2, arena_size/2)
+        z = gaussian(x, y)  # Height determined by the Gaussian function
+
+        # Adjust the size of the rocks based on height. Higher rocks (closer to the peak) will be smaller.
+        size_factor = 1 - (z / mountain_height)
+        size = random.uniform(0.1, max_size) * size_factor
+
+        orientation = p.getQuaternionFromEuler([random.uniform(0, 3.14), random.uniform(0, 3.14), random.uniform(0, 3.14)])
+        rock_shape = p.createCollisionShape(p.GEOM_BOX, halfExtents=[size, size, size])
+        rock_visual = p.createVisualShape(p.GEOM_BOX, halfExtents=[size, size, size], rgbaColor=[0.5, 0.5, 0.5, 1])
+        rock_body = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=rock_shape, baseVisualShapeIndex=rock_visual, basePosition=[x, y, z], baseOrientation=orientation)
+
+
+
+def make_rocks(num_rocks=100, max_size=0.25, arena_size=10):
+    for _ in range(num_rocks):
+        x = random.uniform(-1 * arena_size/2, arena_size/2)
+        y = random.uniform(-1 * arena_size/2, arena_size/2)
+        z = 0.5  # Adjust based on your needs
+        size = random.uniform(0.1,max_size)
+        orientation = p.getQuaternionFromEuler([random.uniform(0, 3.14), random.uniform(0, 3.14), random.uniform(0, 3.14)])
+        rock_shape = p.createCollisionShape(p.GEOM_BOX, halfExtents=[size, size, size])
+        rock_visual = p.createVisualShape(p.GEOM_BOX, halfExtents=[size, size, size], rgbaColor=[0.5, 0.5, 0.5, 1])
+        rock_body = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=rock_shape, baseVisualShapeIndex=rock_visual, basePosition=[x, y, z], baseOrientation=orientation)
+
+
+def make_arena(arena_size=10, wall_height=1):
+    wall_thickness = 0.5
+    floor_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, arena_size/2, wall_thickness])
+    floor_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, arena_size/2, wall_thickness], rgbaColor=[1, 1, 0, 1])
+    floor_body = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=floor_collision_shape, baseVisualShapeIndex=floor_visual_shape, basePosition=[0, 0, -wall_thickness])
+
+    wall_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, wall_thickness/2, wall_height/2])
+    wall_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, wall_thickness/2, wall_height/2], rgbaColor=[0.7, 0.7, 0.7, 1])  # Gray walls
+
+    # Create four walls
+    p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[0, arena_size/2, wall_height/2])
+    p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[0, -arena_size/2, wall_height/2])
+
+    wall_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[wall_thickness/2, arena_size/2, wall_height/2])
+    wall_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[wall_thickness/2, arena_size/2, wall_height/2], rgbaColor=[0.7, 0.7, 0.7, 1])  # Gray walls
+
+    p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[arena_size/2, 0, wall_height/2])
+    p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[-arena_size/2, 0, wall_height/2])
+
+def default_simple_example() :
+    p.connect(p.GUI)
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+    p.setGravity(0, 0, -10)
+
+    arena_size = 20
+    make_arena(arena_size=arena_size)
+
+    #make_rocks(arena_size=arena_size)
+
+    mountain_position = (0, 0, -1)  # Adjust as needed
+    mountain_orientation = p.getQuaternionFromEuler((0, 0, 0))
+    p.setAdditionalSearchPath('shapes/')
+    # mountain = p.loadURDF("mountain.urdf", mountain_position, mountain_orientation, useFixedBase=1)
+    # mountain = p.loadURDF("mountain_with_cubes.urdf", mountain_position, mountain_orientation, useFixedBase=1)
+
+    mountain = p.loadURDF("gaussian_pyramid.urdf", mountain_position, mountain_orientation, useFixedBase=1)
+
+    # generate a random creature
+    cr = creature.Creature(gene_count=3)
+    # save it to XML
+    with open('test.urdf', 'w') as f:
+        f.write(cr.to_xml())
+    # load it into the sim
+    rob1 = p.loadURDF('test.urdf', (0, 0, 10))
+
+    p.setRealTimeSimulation(1)
+
+def run_the_simul_in_the_arena(connect_mode):
+    # generate a random creature
+    cr = creature.Creature(gene_count=3)    
+    sim = simulation.Simulation()
+    if p.GUI == connect_mode:
+        sim.set_as_GUI()
+        print("run as GUI Mode")
+    else:
+        print("run as DIRECT Mode")
+    sim.run_one_creature_in_the_arena(cr, 2400 * 100)
+
+def GA(population_size=10, gene_count=3, iteration_count=1000):
+    print(f'population_size({population_size}), gene_count({gene_count}), iteration_count({iteration_count})')
+    pop = population.Population(pop_size=population_size, 
+                                gene_count=gene_count)
+    #sim = simulation.ThreadedSim(pool_size=1)
+    sim = simulation.Simulation()
+
+    for iteration in range(iteration_count):
+        # this is a non-threaded version 
+        # where we just call run_creature instead
+        # of eval_population
+        for cr in pop.creatures:
+            sim.run_one_creature_in_the_arena(cr, 2400)            
+#            sim.run_creature(cr, 2400)            
+        #sim.eval_population(pop, 2400)
+        fits = [cr.get_distance_travelled() 
+                for cr in pop.creatures]
+        links = [len(cr.get_expanded_links()) 
+                for cr in pop.creatures]
+        print(iteration, "fittest:", np.round(np.max(fits), 3), 
+                "mean:", np.round(np.mean(fits), 3), "mean links", np.round(np.mean(links)), "max links", np.round(np.max(links)))       
+        fit_map = population.Population.get_fitness_map(fits)
+        new_creatures = []
+        for i in range(len(pop.creatures)):
+            p1_ind = population.Population.select_parent(fit_map)
+            p2_ind = population.Population.select_parent(fit_map)
+            p1 = pop.creatures[p1_ind]
+            p2 = pop.creatures[p2_ind]
+            # now we have the parents!
+            dna = genome.Genome.crossover(p1.dna, p2.dna)
+            dna = genome.Genome.point_mutate(dna, rate=0.1, amount=0.25)
+            dna = genome.Genome.shrink_mutate(dna, rate=0.25)
+            dna = genome.Genome.grow_mutate(dna, rate=0.1)
+            cr = creature.Creature(1)
+            cr.update_dna(dna)
+            new_creatures.append(cr)
+        # elitism
+        max_fit = np.max(fits)
+        for cr in pop.creatures:
+            if cr.get_distance_travelled() == max_fit:
+                new_cr = creature.Creature(1)
+                new_cr.update_dna(cr.dna)
+                new_creatures[0] = new_cr
+                filename = "elite_"+str(iteration)+".csv"
+                genome.Genome.to_csv(cr.dna, filename)
+                break
+        
+        pop.creatures = new_creatures
+                        
+if __name__ == '__main__':
+    GA(iteration_count=100)
+#    run_the_simul(p.DIRECT)
+    run_the_simul_in_the_arena(p.GUI)
+#    default_simple_example()
--- MidTerm/simulation.py
+++ MidTerm/simulation.py
@@ -6,7 +6,7 @@ class Simulation:
         self.physicsClientId = p.connect(p.DIRECT)
         self.sim_id = sim_id
 
-    def run_creature(self, cr, iterations=2400):
+    def run_creature_original(self, cr, iterations=2400):
         pid = self.physicsClientId
         p.resetSimulation(physicsClientId=pid)
         p.setPhysicsEngineParameter(enableFileCaching=0, physicsClientId=pid)
@@ -35,7 +35,87 @@ class Simulation:
             #print(pos[2])
             #print(cr.get_distance_travelled())
         
-    
+    def set_as_GUI(self):
+        self.physicsClientId = p.connect(p.GUI)
+
+    def make_arena(self, arena_size=10, wall_height=1):
+        wall_thickness = 0.5
+        floor_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, arena_size/2, wall_thickness], physicsClientId=self.physicsClientId)
+        floor_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, arena_size/2, wall_thickness], rgbaColor=[1, 1, 0, 1], physicsClientId=self.physicsClientId)
+        floor_body = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=floor_collision_shape, baseVisualShapeIndex=floor_visual_shape, basePosition=[0, 0, -wall_thickness], physicsClientId=self.physicsClientId)
+
+        wall_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, wall_thickness/2, wall_height/2], physicsClientId=self.physicsClientId)
+        wall_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[arena_size/2, wall_thickness/2, wall_height/2], rgbaColor=[0.7, 0.7, 0.7, 1], physicsClientId=self.physicsClientId)  # Gray walls
+
+        # Create four walls
+        p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[0, arena_size/2, wall_height/2], physicsClientId=self.physicsClientId)
+        p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[0, -arena_size/2, wall_height/2], physicsClientId=self.physicsClientId)
+
+        wall_collision_shape = p.createCollisionShape(shapeType=p.GEOM_BOX, halfExtents=[wall_thickness/2, arena_size/2, wall_height/2], physicsClientId=self.physicsClientId)
+        wall_visual_shape = p.createVisualShape(shapeType=p.GEOM_BOX, halfExtents=[wall_thickness/2, arena_size/2, wall_height/2], rgbaColor=[0.7, 0.7, 0.7, 1], physicsClientId=self.physicsClientId)  # Gray walls
+
+        p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[arena_size/2, 0, wall_height/2], physicsClientId=self.physicsClientId)
+        p.createMultiBody(baseMass=0, baseCollisionShapeIndex=wall_collision_shape, baseVisualShapeIndex=wall_visual_shape, basePosition=[-arena_size/2, 0, wall_height/2], physicsClientId=self.physicsClientId)
+
+    def make_mountain(self):
+        mountain_position = (0, 0, -1)  # Adjust as needed
+        mountain_orientation = p.getQuaternionFromEuler((0, 0, 0))
+        p.setAdditionalSearchPath('shapes/')
+        # mountain = p.loadURDF("mountain.urdf", mountain_position, mountain_orientation, useFixedBase=1)
+        # mountain = p.loadURDF("mountain_with_cubes.urdf", mountain_position, mountain_orientation, useFixedBase=1)
+
+        mountain = p.loadURDF("gaussian_pyramid.urdf", mountain_position, mountain_orientation, useFixedBase=1, physicsClientId=self.physicsClientId)
+
+    def run_one_creature_in_the_arena(self, cr, iterations=2400):
+        pid = self.physicsClientId
+        p.resetSimulation(physicsClientId=pid)
+        p.setPhysicsEngineParameter(enableFileCaching=0, physicsClientId=pid)
+
+        p.resetDebugVisualizerCamera(
+            cameraDistance=20,
+            cameraYaw=50,
+            cameraPitch=-35,
+            cameraTargetPosition=[0, 0, 0],
+            physicsClientId=pid
+        )
+
+
+        p.setGravity(0, 0, -10, physicsClientId=pid)
+        # plane_shape = p.createCollisionShape(p.GEOM_PLANE, physicsClientId=pid)
+        # floor = p.createMultiBody(plane_shape, plane_shape, physicsClientId=pid)
+
+        self.make_arena(arena_size=20)
+        self.make_mountain()
+        xml_file = 'temp' + str(self.sim_id) + '.urdf'
+        xml_str = cr.to_xml()
+        with open(xml_file, 'w') as f:
+            f.write(xml_str)
+        
+        cid = p.loadURDF(xml_file, physicsClientId=pid)
+
+#        p.resetBasePositionAndOrientation(cid, [0, 0, 2.5], [0, 0, 0, 1], physicsClientId=pid)
+        p.resetBasePositionAndOrientation(cid, [5, 5, 2.5], [0, 0, 0, 1], physicsClientId=pid)
+ 
+
+
+        p.setRealTimeSimulation(1, physicsClientId=pid)
+ 
+
+        for step in range(iterations):
+            p.stepSimulation(physicsClientId=pid)
+            if step % 24 == 0:
+                self.update_motors(cid=cid, cr=cr)
+
+            pos, orn = p.getBasePositionAndOrientation(cid, physicsClientId=pid)
+            cr.update_position(pos)
+            #print(pos[2])
+            #print(cr.get_distance_travelled())
+        print(cr.get_distance_travelled())
+
+
+
+
+
     def update_motors(self, cid, cr):
         """
         cid is the id in the physics engine
@@ -50,8 +130,8 @@ class Simulation:
                     targetVelocity=m.get_output(), 
                     force = 5, 
                     physicsClientId=self.physicsClientId)
+            
         
-
     # You can add this to the Simulation class:
     def eval_population(self, pop, iterations):
         for cr in pop.creatures:
